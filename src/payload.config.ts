1821938d0b436e78f59c0f7430944ae9c5c027e5
“sonia” <soniachaparro21@gmail.com>
2025-03-26 21:21:27 -0300

    Update serverURL in payload configuration to use NEXT_PUBLIC_API_URL environment variable

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -15,7 +15,7 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
-  serverURL: 'https://jopascal-calzados.vercel.app',
+  serverURL: process.env.NEXT_PUBLIC_API_URL,
   admin: {
     user: Users.slug,
     importMap: {
